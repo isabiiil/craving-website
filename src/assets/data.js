602038634253df9a625cdf7ccc961b036a361b41
isabiiil <isabelabonitalla@gmail.com>
2022-03-02 17:45:19 -0500

    created data file

---
+++ src/assets/data.js
@@ -0,0 +1,19 @@
+export const team = [
+  {
+    id: 1,
+    fullName: 'Isabel Abonitalla',
+    title: 'Founder',
+    school: 'CS @ Hunter College',
+  },
+  {
+    id: 2,
+    fullName: 'Jubayer Ahmed',
+    title: 'Frontend Developer',
+    school: 'Daedalus Scholar @ Hunter College',
+  },
+  {
+    id: 3,
+    fullName: 'Tien Nguyen',
+    title: 'Mobile Developer',
+  }
+]
